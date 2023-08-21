fb90f1e6f32c566db69e8e91deec4678654e679f
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-08-21 16:55:11 +0200

    added all the settings to template

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+const path = require('path')
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
+            'resources/scss/app.scss',
+            'resources/js/app.js',
+        ],
+        refresh: true
+    }),
     ],
+
+    resolve: {
+        alias: {
+            '~resources' : '/resources/',
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
+        }
+    }
 });
